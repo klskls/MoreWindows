fd255038c6ae4aa2870ef12dcf68193c0c99357a
klskls <1978354075@qq.com>
2019-01-11 21:22:51 +0800

    1.11

--- src/sort/mergeSort/MergeSort.go
+++ src/sort/mergeSort/MergeSort.go
@@ -2,31 +2,66 @@ package mergeSort
 
 /**
 合并两个有序数组
+https://blog.csdn.net/MoreWindows/article/details/6678165
  */
-func MergeArray(a []int, b []int) [] int {
-	c := make([]int, len(a)+len(b))
-	i, j, k := 0, 0, 0
+func MergeArray(a []int, b []int, c []int) [] int {
+	i, j := 0, 0
 
 	for i < len(a) && j < len(b) {
 		if a[i] < a[j] {
-			c[k] = a[i]
+			c = append(c, a[i])
 			i++
 		} else {
-			c[k] = a[j]
+			c = append(c, a[j])
 			j++
 		}
-		k++
 	}
 
 	for i < len(a) {
-		c[k] = a[i]
-		k++
+		c = append(c, a[i])
 		i++
 	}
 	for j < len(b) {
-		c[k] = a[j]
-		k++
+		c = append(c, a[j])
 		j++
 	}
 	return c
+}
+
+func mergeArray(a []int, first int, mid int, last int, temp [] int) {
+	i, j := first, mid+1
+	k := 0
+	for i <= mid && j <= last {
+		if a[i] < a[j] {
+			temp[k] = a[i]
+			k++
+			i++
+		} else {
+			temp[k] = a[j]
+			k++
+			j++
+		}
+		for i <= mid {
+			temp[k] = a[i]
+			k++
+			i++
+		}
+		for j <= last {
+			temp[k] = a[j]
+			j++
+			k++
+		}
+		for n := 0; n < k; n++ {
+		 	a[first+n]=temp[n]
+		}
+	}
+}
+
+func MergeSort(a []int,first int,last int,temp [] int)  {
+	if first<last{
+		mid:=(first+last)/2
+		MergeSort(a,first,mid,temp)
+		MergeSort(a,mid+1,last,temp)
+		mergeArray(a,first,mid,last,temp)
+	}
 }
--- src/sort/mergeSort/MergeSort_test.go
+++ src/sort/mergeSort/MergeSort_test.go
@@ -8,7 +8,15 @@ import (
 )
 
 func TestMergeArray(t *testing.T) {
-	d:=MergeArray(bubbleSort.BubbleSort(c.ProduceArr(100)), bubbleSort.BubbleSort(c.ProduceArr(100)))
+	d := MergeArray(bubbleSort.BubbleSort(c.ProduceArr(100000)), bubbleSort.BubbleSort(c.ProduceArr(10000)), make([]int, 0))
 	fmt.Println(d)
 	fmt.Println(c.Check(d))
+}
+
+func TestMergeSort(t *testing.T) {
+	s := c.ProduceArr(10)
+	fmt.Println(s)
+	MergeSort(s, 0, len(s)-1, make([]int, len(s)))
+	fmt.Println(s)
+	fmt.Println(c.Check(s))
 }
